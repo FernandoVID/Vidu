7430fd99cf8fb0f1c95a48bfcca8950e2c50a84d
Viduranga Fernando <Viduranga Fernando@192.168.2.207>
2018-12-22 17:25:25 -0500

    hgwe

--- GitTest/src/HelloWorld.java
+++ GitTest/src/HelloWorld.java
@@ -4,14 +4,14 @@ public class HelloWorld {
 	public static void main(String args[]) {
 		while(true) {
 		try {
-			Thread.sleep(1000);
+			Thread.sleep(1);
 		} catch (InterruptedException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		long number = System.nanoTime();
+		long number = System.nanoTime() ;
 		
-		System.err.println(number);
+		System.err.println(number * System.nanoTime());
 		}
 	}
 	
